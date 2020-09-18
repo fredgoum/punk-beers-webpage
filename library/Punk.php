df0f1125ceb6aa3e5363a84f20b8874a6ccde02c
root <root@DESKTOP-SP494RK.localdomain>
2020-09-18 21:03:01 +0200

    get client API datas with cURL

--- library/Punk.php
+++ library/Punk.php
@@ -8,14 +8,27 @@ Class Punk {
     // What I've to do ?
   }
 
+  // Get the API data of the 60 beers to display
   public function getBeers()
   {
-    // What I've to do ?
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "https://api.punkapi.com/v2/beers?page=4&per_page=2");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($curl);
+    curl_close($curl);
+    echo($output);
+    return $output;
   }
 
+  // Get the API data of a beer
   public function getBeer($id)
   {
-    // What I've to do ?
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, "https://api.punkapi.com/v2/beers/{$id}");
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($curl);
+    curl_close($curl);
+    echo($output);
+    return $output;
   }
 }
-
--- public/endpoint.php
+++ public/endpoint.php
@@ -1,2 +1,15 @@
 <?php
-// What I've to do ?
+  include('../library/Punk.php');
+  $punk = new Punk();
+
+  if($_GET["request"] == "getBeers" && !isset($_GET["id"])) {
+
+    $beers = $punk->getBeers();
+
+  } else if ($_GET["request"] == "getBeer" && isset($_GET["id"])) {
+
+    $id = $_GET["id"];
+    $beer = $punk->getBeer($id);
+    
+  }
+?>
